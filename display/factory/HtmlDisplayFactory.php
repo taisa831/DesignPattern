b7194652e3691846f8c3ea73f17e7ff05fbe7bd6
taisa <g5.taisa831@gmail.com>
2017-05-10 15:43:58 +0900

    更新

--- display/factory/HtmlDisplayFactory.php
+++ display/factory/HtmlDisplayFactory.php
@@ -4,7 +4,6 @@ require_once dirname(__FILE__) . '/../../framework/factory/DisplayFactory.php';
 
 class HtmlDisplayFactory extends DisplayFactory
 {
-
     public function createDisplay($char)
     {
         return new HtmlDisplay($char);
@@ -14,5 +13,5 @@ class HtmlDisplayFactory extends DisplayFactory
     {
         $this->displays[] = $display;
     }
-
 }
+
